8443e81825daacaec507aee17848de7c1aac15f6
CaffeineAddict <sixtenthelin@icloud.com>
2024-01-19 23:07:40 +0100

    Look around

---
+++ FirstPersonPlatformer/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
